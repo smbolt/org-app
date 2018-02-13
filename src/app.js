1bbd274d695dd7d5cef60d3151651fcc07b88823
Stephen Bolt on NB-99 <stephen.m.bolt@gmail.com>
2018-02-12 21:00:51 -0600

    Starting to consume org-gs.

---
+++ src/app.js
@@ -0,0 +1,14 @@
+var gsLib = require('org-gs');
+var gs = gsLib('abc');
+
+console.log('app starting');
+console.log('gsLib = ' + gsLib);
+console.log('gs = ' + gs);
+console.log('gs.prop1 = ' + gs.prop1);
+console.log('gs.prop2 = ' + gs.prop2);
+console.log('gs.method1 = ' + gs.method1());
+
+
+//console.log(x);
+
+console.log('app ending');
